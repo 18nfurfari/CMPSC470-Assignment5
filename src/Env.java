9c948154e0b3968b889e56816d58b59d8eeccd0c
18nfurfari <112352692+18nfurfari@users.noreply.github.com>
2023-04-17 19:03:45 -0400

    first commit

---
+++ src/Env.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Env
+{
+    HashMap<String, Object> sym_table;
+
+    public Env prev;
+    public Env(Env p)
+    {
+        prev=p;
+        sym_table=new HashMap<String, Object>();
+        if (p!=null)
+        {
+            PutAll(p);
+        }
+    }
+    public void Put(String name, Object value)
+    {
+        sym_table.put(name, value);
+    }
+
+    public void PutAll(Env p)
+    {
+        sym_table.putAll(p.sym_table);
+    }
+
+    public Object Get(String name)
+    {
+        // this is a fake implementation
+        // For the real implementation, I recommend to return a class object
+        //   since the identifier's type can be variable or function
+        //   whose detailed attributes will be different
+//        if(name.equals("a") == true) return "int";
+//        if(name.equals("b") == true) return "bool";
+//        if(name.equals("testfunc") == true) return "func()->int";
+//        return null;
+        if (sym_table.containsKey(name))
+        {
+            return sym_table.get(name);
+        }
+        return null;
+    }
+}
